fc6a37226c0a78c50b0b6876b3a5695932cea1e3
poojanirmal <nirmalpooja0597@gmail.com>
2023-10-17 21:44:54 -0700

    pattern printing java file added

---
+++ src/test/java/PatternPrinting/StarPattern.java
@@ -0,0 +1,141 @@
+package PatternPrinting;
+
+public class StarPattern {
+
+	public static void main(String args[])
+	{
+//		Star pattern
+//		1.Triangle pattern
+//		*
+//		**
+//		***
+//		****
+//		*****
+		for(int i =0;i<=4;i++)
+		{
+		 for(int j=0;j<=i;j++)
+		{
+		 System.out.print("*");
+		}
+		 System.out.println(" ");
+		}
+
+		System.out.println(".......................");
+		
+//		2.Reverse Triangle Pattern
+//		*****
+//		****
+//		***
+//		**
+//		*
+		for(int i=0;i<=4;i++)
+		{
+		 for(int j=4;j>=i;j--)
+		{
+		 System.out.print("*");
+		}
+		 System.out.println(" ");
+		}
+
+		System.out.println(".......................");
+		
+//		3.Both Combine
+//		*
+//		**
+//		***
+//		****
+//		*****
+//		****
+//		***
+//		**
+//		*
+		for(int i=0;i<=4;i++)
+		{
+		 for(int j=0;j<=i;j++)
+		{
+		 System.out.print("*");
+		}
+		 System.out.println(" ");
+		}
+
+		for(int i=0;i<=4;i++)
+		{
+		 for(int j=4;j>i;j--)
+		{
+		 System.out.print("*");
+		}
+		 System.out.println(" ");
+		}
+
+		System.out.println(".......................");
+		
+//		4.Star With Space In Triangle Form
+//		---*
+//		--**
+//		-***
+//		****
+		for(int i=1;i<=4;i++)
+		{
+		 for(int j=3;j>=i;j--)
+		{
+		 System.out.print(" ");
+		}
+		 for(int k=1;k<=i;k++)
+		{
+		 System.out.print("*");
+		}
+		 System.out.println(" ");
+		}
+
+		System.out.println(".......................");
+		
+//		5.Reverse of Star with Space
+//		****
+//		-***
+//		--**
+//		---*
+		for(int i=1;i<=4;i++)
+		{
+		 for(int j=1;j<i;j++)
+		{
+		 System.out.print(" ");
+		}
+		for(int k=4;k>=i;k--)
+		{
+		 System.out.print("*");
+		}
+		System.out.println(" ");
+		}
+		
+		System.out.println(".......................");
+		
+//		6.Both combine
+		
+		for(int i=1;i<=4;i++)
+		{
+		 for(int j=3;j>=i;j--)
+		{
+		 System.out.print(" ");
+		}
+		 for(int k=1;k<=i;k++)
+		{
+		 System.out.print("*");
+		}
+		 System.out.println(" ");
+		}
+		for(int i=1;i<=4;i++)
+		{
+		 for(int j=1;j<i;j++)
+		{
+		 System.out.print(" ");
+		}
+		 
+		 
+		for(int k=4;k>=i;k--)
+		{
+		 System.out.print("*");
+		}
+		System.out.println(" ");
+		}
+	}
+}
